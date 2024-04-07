79b2ec459d729264cde0df97d9f26c7d157de7d6
edsplore <138727039+edsplore@users.noreply.github.com>
2024-04-07 14:34:05 +0530

    hide modelselect

--- client/src/components/Chat/Input/HeaderOptions.tsx
+++ client/src/components/Chat/Input/HeaderOptions.tsx
@@ -69,13 +69,13 @@ export default function HeaderOptions() {
       <Anchor>
         <div className="my-auto lg:max-w-2xl xl:max-w-3xl">
           <span className="flex w-full flex-col items-center justify-center gap-0 md:order-none md:m-auto md:gap-2">
-            {/* <div className="z-[61] flex w-full items-center justify-center gap-2">
-              <ModelSelect
+            <div className="z-[61] flex w-full items-center justify-center gap-2">
+              {/* <ModelSelect
                 conversation={conversation}
                 setOption={setOption}
                 isMultiChat={true}
                 showAbove={false}
-              />
+              /> */}
               {!noSettings[endpoint] && (
                 <Button
                   type="button"
@@ -89,7 +89,7 @@ export default function HeaderOptions() {
                   <Settings2 className="w-4 text-gray-600 dark:text-white" />
                 </Button>
               )}
-            </div> */}
+            </div>
             {/* <OptionsPopover
               visible={showPopover}
               saveAsPreset={saveAsPreset}
